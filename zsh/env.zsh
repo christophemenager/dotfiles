64b83404a1afbca6e886e37927b4c608dfba4a35
Christophe Menager <christophe.menager@matera.eu>
2021-03-01 16:42:58 +0100

    Update MARCH

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,6 +7,8 @@ export SUDO_EDITOR='nvim'
 export ANDROID_HOME=/Users/christophemenager/Library/Android/sdk
 export ANDROID_SDK_ROOT=$ANDROID_HOME
 
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
 export BASE16_SHELL="$HOME/.config/base16-shell/" # Base 16
 [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
@@ -18,9 +20,13 @@ export LANG=en_US.UTF-8 # Fastlane config
 # Path
 export PATH=$PATH:/Users/christophemenager/Library/Android/sdk/platform-tools # Android
 export PATH=$PATH:/Users/christophemenager/Library/Android/sdk/tools # Android
+export PATH=$PATH:/Users/christophemenager/Library/Android/sdk/tools/bin
+export PATH=$PATH:/usr/local/bin/adb
 export PATH=$PATH:/Users/christophemenager/Library/Flutter/bin # Flutter
 export PATH="$PATH:$HOME/.rvm/bin" #RVM
 export PATH="$PATH:$HOME/Library/Python/3.7/bin" # python
+export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
+export PATH=$PATH:$JAVA_HOME/bin
+
 
 typeset -U PATH # Remove duplicates in $PATH
-
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -23,3 +23,10 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
